62aba71a48a3b0f26d72341126f2361f5825ac72
Pakmanlie <62767966+Pakmanlie@users.noreply.github.com>
2022-02-13 16:06:18 -0500

    qwe
    we

---
+++ New Unity Project (1)/Assets/Void.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Void : DragObject
+{
+    public override void attach(GameObject attachingGameObject)
+    {
+    }
+    public override void fuse(GameObject gameObjectFuseTo)
+    {
+            Destroy(gameObjectFuseTo);    
+    }
+}
